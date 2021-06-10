b54e6ecd940603f8d4d6a582eee686158f659ca1
Josephine But <josephinebut@Josephines-MacBook-Air.local>
2021-06-10 15:18:23 -0700

    Add Admin features include edit and delete

--- app/models.py
+++ app/models.py
@@ -2,25 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager
+from sqlalchemy import exc
 
-login = LoginManager ( )
-db = SQLAlchemy ( )
+login = LoginManager()
+db = SQLAlchemy()
 
-class UserModel (UserMixin, db.Model):
+
+class UserModel(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(80))
     lastname = db.Column(db.String(80))
-    email = db.Column(db.String (80), unique=True)
-    username = db.Column(db.String (100), unique=True)
-    password = db.Column(db.String ( ))
+    email = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(20))
+    role = db.Column(db.String(20))
+
+    def __init__(self, firstname='', lastname='', email='', username='', password='', role=''):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.username = username
+        self.password = password
+        self.role = role
 
     def set_password(self, password):
-        self.password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8);
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+        return self.password
 
     def check_password(self, password):
-        return check_password_hash(self.password, password);
+        return check_password_hash(self.password, password)
 
     def check_username_exist(self, username):
         return UserModel.query.filter_by(username=username).first()
@@ -28,19 +40,57 @@ class UserModel (UserMixin, db.Model):
     def check_email_exist(self, email):
         return UserModel.query.filter_by(email=email).first()
 
-    def add_user(self, user, app):
+    def add_user(self, user):
         try:
             db.session.add(user)
             db.session.flush()
-        except exceptions.SQLAlchemyError:
+        except exc.SQLAlchemyError:
             db.session.rollback()
-            app.logger.error("DB rollback for user - ", user.username)
             return False
         else:
             db.session.commit()
-            app.logger.info('User is added successfully! - ', user.username)
             return True
+
+    def check_admin(self, username):
+        if UserModel.query.filter_by(username=username, role='admin').first():
+            return True
+        else:
+            return False
+
 
 @login.user_loader
 def load_user(id):
     return UserModel.query.get(int(id))
+
+
+class Admin(UserModel):
+
+    def retrieve_user(self, username):
+        user = UserModel.query.filter_by(username=username).first()
+        return user
+
+    def delete_user(self, id):
+        user = UserModel.query.get_or_404(id)
+        try:
+            db.session.delete(user)
+            db.session.flush()
+        except exc.SQLAlchemyError:
+            db.session.rollback()
+            return False
+        else:
+            db.session.commit()
+            return True
+
+    def add_admin_user(self, firstname, lastname, email, username, password):
+        admin_user = Admin(firstname, lastname, email,
+                           username, password, 'admin')
+        admin_user.password = admin_user.set_password(admin_user.password)
+        try:
+            db.session.add(admin_user)
+            db.session.flush()
+        except exc.SQLAlchemyError:
+            db.session.rollback()
+            return False
+        else:
+            db.session.commit()
+            return True
--- app/registerLogin.py
+++ app/registerLogin.py
@@ -1,52 +1,72 @@
 from flask import Flask, render_template, request, redirect, flash
 from flask_login import login_required, current_user, login_user, logout_user
-from models import UserModel, db, login
+from models import UserModel, db, login, Admin
 
-app = Flask (__name__)
-app.secret_key = 'xyz'
+app = Flask(__name__)
+app.secret_key = 'xf7\xc4\xfa\x91'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app (app)
-login.init_app (app)
+db.init_app(app)
+login.init_app(app)
 login.login_view = 'login'
+
+user = UserModel()
+admin = Admin()
 
 
 @app.before_first_request
 def create_all():
-    db.create_all ( )
+    db.create_all()
+    admin.add_admin_user('John', 'Boyle', 'admin1@123.com', 'admin1', 'admin1')
+    admin.add_admin_user('Stephen', 'Best', 'admin2@123.com', 'admin2', 'admin2')
 
 
-@app.route ('/index')
+@app.route('/index')
 @login_required
 def index():
-    return render_template ('index.html')
+    return render_template('index.html')
 
 
-@app.route ('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    msg=''
+    msg = ''
     if current_user.is_authenticated:
-        return redirect ('/index')
+        return redirect('/index')
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user=UserModel(username=username, password=password )
-        user = user.check_username_exist(username)
-        if user is not None and user.check_password(password):
-            login_user(user)
-            return redirect('/index')
+        user = UserModel.query.filter_by(username=username).first()
+        if user is not None:
+            if user.role == 'customer':
+                user = UserModel(user.firstname, user.lastname, user.email,
+                                user.username, user.password, user.role)
+
+                path = '/index'
+            else:
+                user = Admin(user.firstname, user.lastname, user.email,
+                             user.username, user.password, user.role)
+                path = '/admin/data'
+            user = user.check_username_exist(username)
+            if user.check_password(password):
+                login_user(user)
+                return redirect(path)
+            else:
+                msg = 'Incorrect password!'
         else:
-            msg = 'Incorrect username/password!'
+            msg = 'Incorrect username!'
+
     return render_template('login.html', msg=msg)
 
 
-@app.route ('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    msg = ' '
+    role = 'customer'
     if current_user.is_authenticated:
-        return redirect ('/index')
+        return redirect('/index')
 
     if request.method == 'POST':
         firstname = request.form['firstname']
@@ -55,21 +75,80 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        user=UserModel(firstname=firstname, lastname=lastname,email=email, username=username)
-        if user.check_username_exist(username):
+        customer = UserModel(firstname, lastname, email,
+                            username, password, role)
+        if customer.check_username_exist(username):
             msg = 'Username is already exist'
             return render_template('register.html', msg=msg)
 
-        if user.check_email_exist(email):
+        if customer.check_email_exist(email):
             msg = 'Email is already exist'
             return render_template('register.html', msg=msg)
 
-        user.set_password(password)
-        if user.add_user(user, app):
-            return redirect ('/login')
-    return render_template ('register.html')
+        customer.set_password(password)
+        if customer.add_user(customer):
+            msg = 'User is added successfully!'
+            return redirect('/login')
+        else:
+            msg = 'Failed to add the user!'
+    return render_template('register.html')
 
-@app.route ('/logout')
+
+@app.route('/logout')
+@login_required
 def logout():
-    logout_user ( )
-    return redirect ('/index')
+    logout_user()
+    return redirect('/index')
+
+
+@app.route('/admin/data/', methods=['GET', 'POST'])
+@login_required
+def get_user_data():
+    if request.method == 'GET':
+        return render_template('userinput.html')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        return redirect(f'/admin/data/{username}')
+
+
+@app.route('/admin/data/<string:username>', methods=['GET', 'POST'])
+@login_required
+def display_user_detail(username):
+    user1 = (Admin(user)).retrieve_user(username)
+    if user1:
+        return render_template('userlist.html', user=user1)
+    else:
+        return redirect(f'/admin/data/')
+
+
+@app.route('/admin/data/update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update_user_record(id):
+    user = UserModel.query.get_or_404(id)
+    if request.method == 'POST':
+        user.firstname = request.form['firstname']
+        user.lastname = request.form['lastname']
+        user.username = request.form['username']
+        user.password = request.form['password']
+        user.password = user.set_password(user.password)
+        user.email = request.form['email']
+
+        try:
+            db.session.commit()
+            return redirect(f'/admin/data/{user.username}')
+        except:
+            return "Problem to updating the user record."
+    else:
+        return render_template('userupdate.html', user=user)
+
+
+@app.route('/admin/data/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_user_record(id):
+    if (Admin(user)).delete_user(id):
+        return redirect('/admin/data')
+    else:
+        return "Problem to deleting the user record."
+
+
