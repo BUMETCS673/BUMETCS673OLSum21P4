52caa16e7677b04ee20a3637675f8cd64141695a
Daniel Graziano <daniel@deepcure.ai>
2021-06-11 14:35:03 -0400

    feat: add calorie retrieval (for 2 foods only) for total meal calorie logging

--- models.py
+++ models.py
@@ -7,12 +7,12 @@ db = SQLAlchemy()
 
 
 class MealModel(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  meal_type = db.Column(db.String(200), nullable=False)
-  food_item1 = db.Column(db.String(200), nullable=False)
-  food_item2 = db.Column(db.String(200), nullable=False)
-  date_created = db.Column(db.DateTime, default=datetime.utcnow) 
+    id = db.Column(db.Integer, primary_key=True)
+    meal_type = db.Column(db.String(200), nullable=False)
+    food_item1 = db.Column(db.String(200), nullable=False)
+    food_item2 = db.Column(db.String(200), nullable=False)
+    calories = db.Column(db.Float(), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-  def __repr__(self):
-    return '<Task %r>' % self.id
-
+    def __repr__(self):
+        return '<Task %r>' % self.id
