62a552261c8c0ece7d95426646b874c98da3a7bf
Daniel Graziano <daniel@deepcure.ai>
2021-06-12 19:51:44 -0400

    refactor: remove idea folder and add back in test_usda functions from stash

---
+++ app/test_usda.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+from usda import extract_avg_calorie_data, usda_api_call, load_cfg
+from pyprojroot import here
+
+
+class TestUSDAMethods(unittest.TestCase):
+
+    def test_load_cfg(self):
+
+        self.assertTrue(os.path.isfile(here() / 'user_config.yml'))
+        cfg = load_cfg()
+        self.assertEqual(type(cfg), dict)
+        self.assertIn('usda', cfg.keys())
+        self.assertIn('api_key', cfg['usda'].keys())
+        self.assertIsNotNone(cfg['usda']['api_key'])
+
+    def test_usda_api_call(self):
+        json_response = usda_api_call('apple', load_cfg())
+
+
+if __name__ == '__main__':
+    unittest.main()
