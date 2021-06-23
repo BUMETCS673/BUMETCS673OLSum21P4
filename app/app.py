6f8cf5811e438cf44f75c48e855ac84d83d19342
Josephine But <josephinebut@Josephines-MacBook-Air.local>
2021-06-22 21:12:47 -0700

    to remove admin credientials

--- app/app.py
+++ app/app.py
@@ -29,23 +29,9 @@ def handle_error(err):
         return render_template('validationError.html', message=messages)
 
 
-# path
-path = '../database'
-if not os.path.isdir(path):
-    try:
-        os.mkdir(path)
-    except OSError as error:
-        print("Cannot create the database folder.")
-
 user = UserModel()
 admin = Admin()
 db.init_app(app)  # initalize db
-with app.app_context():
-    db.create_all()
-    admin.add_admin_user('John', 'Boyle', 'admin1@123.com', 'admin1', 'admin1')
-    admin.add_admin_user('Stephen', 'Best', 'admin2@123.com', 'admin2', 'admin2')
-    # db.create_all(bind='two')
-    db.session.commit()
 
 # import declared routes
 import routes
